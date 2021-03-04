360b823043cf4f2cece1e06d372b439de498e070
Julien Salinas <all@juliensalinas.com>
2021-03-04 20:30:03 +0000

    Update Hugging Face endpoints.

--- lib/nlpcloud.rb
+++ lib/nlpcloud.rb
@@ -16,34 +16,67 @@ module NLPCloud
       @root_url = "#{BASE_URL}/#{API_VERSION}/#{model}"
     end
 
-    def entities(user_input)
-      api_post('entities', user_input)
-    end
-
-    def dependencies(user_input)
-      api_post('dependencies', user_input)
-    end
-
-    def sentence_dependencies(user_input)
-      api_post('sentence-dependencies', user_input)
-    end
-
-    def lib_versions
-      api_get('version')
-    end
-
-    private
-
-    def api_post(endpoint, user_input)
+    def entities(text)
       payload = {
-        'text' => user_input
+        'text' => text
       }
-      response = RestClient.post("#{@root_url}/#{endpoint}", payload.to_json, @headers)
+      response = RestClient.post("#{@root_url}/entities", payload.to_json, @headers)
       JSON.parse(response.body)
     end
 
-    def api_get(endpoint)
-      response = RestClient.get("#{@root_url}/#{endpoint}", @headers)
+    def classification(text, labels, multi_class)
+      payload = {
+        'text' => text,
+        'labels' => labels,
+        'multi_class' => multi_class
+      }
+      response = RestClient.post("#{@root_url}/classification", payload.to_json, @headers)
+      JSON.parse(response.body)
+    end
+
+    def sentiment(text)
+      payload = {
+        'text' => text
+      }
+      response = RestClient.post("#{@root_url}/sentiment", payload.to_json, @headers)
+      JSON.parse(response.body)
+    end
+
+    def question(context, question)
+      payload = {
+        'context' => context,
+        'question' => question
+      }
+      response = RestClient.post("#{@root_url}/question", payload.to_json, @headers)
+      JSON.parse(response.body)
+    end
+
+    def summarization(text)
+      payload = {
+        'text' => text
+      }
+      response = RestClient.post("#{@root_url}/summarization", payload.to_json, @headers)
+      JSON.parse(response.body)
+    end
+
+    def dependencies(text)
+      payload = {
+        'text' => text
+      }
+      response = RestClient.post("#{@root_url}/dependencies", payload.to_json, @headers)
+      JSON.parse(response.body)
+    end
+
+    def sentence_dependencies(text)
+      payload = {
+        'text' => text
+      }
+      response = RestClient.post("#{@root_url}/sentence-dependencies", payload.to_json, @headers)
+      JSON.parse(response.body)
+    end
+
+    def lib_versions
+      response = RestClient.get("#{@root_url}/versions", @headers)
       JSON.parse(response.body)
     end
   end
